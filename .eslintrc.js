df25d60cbfab906571f491920b6959a5e5012814
Keisuke Kan <9renpoto@gmail.com>
2016-11-11 21:30:26 +0900

    feat(package) add @9renpoto/eslint-config-flowtype

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "plugins": [
-    "flowtype"
-  ],
   "extends": [
-    "@9renpoto/eslint-config",
-    "plugin:flowtype/recommended"
+    "@9renpoto/eslint-config-flowtype"
   ]
 }
