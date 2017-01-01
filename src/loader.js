72120576ae092537c3238b5a4963331989979b14
Keisuke Kan <9renpoto@gmail.com>
2017-01-01 15:42:23 +0900

    chore(src) add loader

---
+++ src/loader.js
@@ -0,0 +1,4 @@
+const script = document.createElement('script')
+const filename = Math.random() > 0.5 ? 'webpack' : 'browserify'
+script.src = `https://9renpoto.github.io/js/${filename}.bundle.js`
+document.body.appendChild(script)
