8e1b3292600709d1606d5a1621b0eec470bcb8ca
Keisuke Kan <9renpoto@gmail.com>
2017-03-27 03:04:46 +0900

    fix(package) fix coverage options

--- webpack/module.js
+++ webpack/module.js
@@ -2,6 +2,28 @@
 
 module.exports = {
   loaders: [
-    { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      options: {
+        'plugins': [
+          'transform-flow-strip-types'
+        ],
+        'presets': [
+          'latest'
+        ],
+        'env': {
+          'development': {
+            'presets': [
+              'power-assert'
+            ],
+            'plugins': [
+              'istanbul'
+            ]
+          }
+        }
+      }
+    }
   ]
 }
