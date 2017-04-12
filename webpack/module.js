e29d721f7544de9f6d07ec823b037af112e9646c
Keisuke Kan <9renpoto@gmail.com>
2017-04-12 11:00:05 +0900

    refactor(package) update deps

--- webpack/module.js
+++ webpack/module.js
@@ -5,25 +5,7 @@ module.exports = {
     {
       test: /\.js$/,
       loader: 'babel-loader',
-      exclude: /node_modules/,
-      options: {
-        'plugins': [
-          'transform-flow-strip-types'
-        ],
-        'presets': [
-          'latest'
-        ],
-        'env': {
-          'development': {
-            'presets': [
-              'power-assert'
-            ],
-            'plugins': [
-              'istanbul'
-            ]
-          }
-        }
-      }
+      exclude: /node_modules/
     }
   ]
 }
