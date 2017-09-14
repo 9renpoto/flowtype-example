e992d34fff4142c717edb6de159074db28bed4e1
Keisuke Kan <9renpoto@gmail.com>
2017-09-14 22:06:17 +0900

    chore(package) fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,16 @@
 /* @flow */
 module.exports = {
   extends: ['@9renpoto/eslint-config-flowtype', 'plugin:fp/recommended'],
-  plugins: ['fp']
+  plugins: ['fp'],
+  rules: {
+    'fp/no-nil': 1,
+    'fp/no-unused-expression': 1,
+    'fp/no-mutation': [
+      2,
+      {
+        commonjs: true,
+        allowThis: true
+      }
+    ]
+  }
 }
