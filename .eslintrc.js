f05bda5112d3353f8238ac57c789d22531d224cb
Keisuke Kan <9renpoto@gmail.com>
2017-11-25 16:26:21 +0900

    Feature/react (#131)
    * chore(package) add react sample
    
    * chore(package) add react sample
    
    * fix(packages/jest) Added scripts
    
    * chore(travis) add open jdk

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,11 @@
 /* @flow */
 module.exports = {
-  extends: ['@9renpoto/eslint-config-flowtype', 'plugin:fp/recommended'],
-  plugins: ['fp'],
-  rules: {
-    'fp/no-nil': 1,
-    'fp/no-unused-expression': 1,
-    'fp/no-mutation': [
-      2,
-      {
-        commonjs: true,
-        allowThis: true
-      }
-    ]
+  extends: [
+    '@9renpoto/eslint-config-react',
+    '@9renpoto/eslint-config-flowtype'
+  ],
+  plugins: ['jest'],
+  env: {
+    'jest/globals': true
   }
 }
