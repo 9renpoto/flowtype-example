2b23b40340b19bb124ff94756614f7d7bea001c8
Keisuke Kan <9renpoto@gmail.com>
2018-03-18 13:35:00 +0900

    use yarn workspace (#187)
    * use yarn workspace
    
    * :thinking:

--- packages/wdio/wdio.conf.js
+++ packages/wdio/wdio.conf.js
@@ -1,38 +1,22 @@
 exports.config = {
   specs: ['./test/*.js'],
-  exclude: [],
-  maxInstances: 10,
   capabilities: [
     {
-      maxInstances: 5,
-      browserName: 'firefox'
-    },
-    {
-      maxInstances: 5,
       browserName: 'chrome'
     },
     {
-      maxInstances: 5,
       browserName: 'phantomjs'
     }
   ],
   sync: true,
-  logLevel: 'silent',
-  coloredLogs: true,
   bail: 0,
   outputDir: './reports',
   screenshotPath: './errorShots/',
   baseUrl: 'http://localhost',
-  waitforTimeout: 10000,
-  connectionRetryTimeout: 90000,
-  connectionRetryCount: 3,
-  services: ['firefox-profile', 'selenium-standalone', 'phantomjs'],
+  services: ['selenium-standalone', 'phantomjs'],
   framework: 'mocha',
-  reporters: ['dot', 'spec', 'junit', 'allure', 'teamcity', 'json', 'concise'],
+  reporters: ['dot', 'spec'],
   reporterOptions: {
-    junit: {
-      outputDir: './reports'
-    },
     json: {
       outputDir: './reports'
     }
